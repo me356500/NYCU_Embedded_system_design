f22fe83f9e8376b8bb872b50ce715a7b9fc60760
xlpenix <jlxuugu2@gmail.com>
2023-11-09 05:26:13 -0800

    lab4 basic

--- lab4/tmp.cpp
+++ lab4/tmp.cpp
@@ -69,8 +69,8 @@ void screenshot(cv::Mat& frame) {
 
 mutex mutex_screenshot;
 bool flag_screenshot = 0, flag_end = 0;
-int shift_val = 160;
-bool mvleft = 0, mvright = 0;
+
+
 void listen_keyboard_terminal() {
 
     //cout << "Press 'c' to screenshot\nPress 'Esc' to end the program\n";
@@ -100,20 +100,10 @@ void listen_keyboard_terminal() {
         if (read(STDIN_FILENO, &key, 1) == 1) {
             //cout << key << '\n';
         }
-        // 480
         
-        if (key == 'j') {
+        if (key == 'c') {
             mutex_screenshot.lock();
-            mvleft = 1;
-            shift_val -= 20;
-            shift_val = max(-480, shift_val);
-            mutex_screenshot.unlock();
-        }
-        if (key == 'l') {
-            mutex_screenshot.lock();
-            mvright = 1;
-            shift_val += 20;
-            shift_val = min(960, shift_val);
+            flag_screenshot = 1;
             mutex_screenshot.unlock();
         }
         else if (key == 27) {
@@ -126,8 +116,7 @@ void listen_keyboard_terminal() {
 
 }
 
-
-
+int start = 0;
 int print_frame(cv::Mat& frame, std::ofstream& ofs, struct framebuffer_info& fb_info) {
 
     cv::Size2f frame_size = frame.size();
@@ -136,10 +125,10 @@ int print_frame(cv::Mat& frame, std::ofstream& ofs, struct framebuffer_info& fb_
     int fb_width = fb_info.xres_virtual;
     int pixel_bytes = fb_info.bits_per_pixel / 8;
 
-    for (int i = 0; i < frame_size.height; ++i) {
+    for (int i = start; i < frame_size.height; ++i) {
         // move ofs to ith row of framebuffer
         
-        ofs.seekp(i * pixel_bytes * fb_width + shift_val);
+        ofs.seekp(i * pixel_bytes * fb_width + 640);
         // writing row by row
         // reinterpret : uchar* to char*
         ofs.write(reinterpret_cast<char*>(frame.ptr(i)), pixel_bytes * frame_size.width);
@@ -153,6 +142,12 @@ int main(int argc, char **argv) {
     // variable to store the frame get from video stream
     cv::Mat frame;
 
+    // open video stream device
+    // https://docs.opencv.org/3.4.7/d8/dfe/classcv_1_1VideoCapture.html#a5d5f5dacb77bbebdcbfb341e3d4355c1
+    // camera id ??
+    // camera_id + domain_offset (CAP_*) id of the video capturing device to open.
+    cv::VideoCapture camera (2);
+
     // get info of the framebuffer
     framebuffer_info fb_info = get_framebuffer_info("/dev/fb0");
 
@@ -160,31 +155,28 @@ int main(int argc, char **argv) {
     // http://www.cplusplus.com/reference/fstream/ofstream/ofstream/
     std::ofstream ofs("/dev/fb0");
 
+    // check if video stream device is opened success or not
+    // https://docs.opencv.org/3.4.7/d8/dfe/classcv_1_1VideoCapture.html#a9d2ca36789e7fcfe7a7be3b328038585
+    if (!camera.isOpened()) {
+        std::cerr << "Could not open camera.\n";
+        return 1;
+    }
 
+    // set frame property
+    double frame_rate = 30.0;
+    
+    // scale 10 : 6
+    double frame_width = atof(argv[1]);
+    double frame_height = atof(argv[2]);
+    start = atoi(argv[3]);
+    // https://docs.opencv.org/3.4.7/d8/dfe/classcv_1_1VideoCapture.html#a8c6d8c2d37505b5ca61ffd4bb54e9a7c
+    // https://docs.opencv.org/3.4.7/d4/d15/group__videoio__flags__base.html#gaeb8dd9c89c10a5c63c139bf7c4f5704d
+    camera.set(CV_CAP_PROP_FRAME_WIDTH, frame_width);
+    camera.set(CV_CAP_PROP_FRAME_HEIGHT, frame_height);
+    camera.set(CV_CAP_PROP_FPS, frame_rate);
 
     // bonus
     thread t_listen(listen_keyboard_terminal);
-
-    int cvtcode;
-    
-    if (filename.substr(filename.size() - 3) == "bmp") {
-        image = cv::imread(filename, cv::IMREAD_COLOR);
-        cvtcode = cv::COLOR_BGR2BGR565;
-    }
-    else {
-        cout << "png\n";
-        image = cv::imread(filename, cv::IMREAD_UNCHANGED);
-        cvtcode = cv::COLOR_BGRA2BGR565;
-    }
-    
-
-    // opencv mat size
-    image_size = image.size();
-    cout << "img size : " << image_size << '\n';
-
-    // BGR to BGR565 (16-bit image)
-    // bmp : no compression using BGR
-    cv::cvtColor(image, image_convert, cvtcode);
 
     while (1) {
 
@@ -193,21 +185,14 @@ int main(int argc, char **argv) {
         // camera.read
         // https://docs.opencv.org/3.4.7/d8/dfe/classcv_1_1VideoCapture.html#a199844fb74226a28b3ce3a39d1ff6765
         // read next video
+        camera.read(frame);
 
 
-
-        if (mvleft) {
-            //screenshot(frame);
+        if (flag_screenshot) {
+            screenshot(frame);
             mutex_screenshot.lock();
-            mvleft = 0;
+            flag_screenshot = 0;
             mutex_screenshot.unlock();
-        }
-
-        if (mvright) {
-            mutex_screenshot.lock();
-            mvright = 0;
-            mutex_screenshot.unlock();
-
         }
 
         if (flag_end) {
@@ -215,7 +200,8 @@ int main(int argc, char **argv) {
             break;
         }
 
-        print_image(frame, ofs, fb_info);
+        cv::cvtColor(frame, frame, cv::COLOR_BGR2BGR565);
+        print_frame(frame, ofs, fb_info);
 
     }
 
